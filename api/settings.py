0ce415cbb1afaa1746cb9386017ae8e15f80ff64
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-04-19 20:33:48 +0000

    Still haven't worked too much on the models

--- api/settings.py
+++ api/settings.py
@@ -181,7 +181,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
     'ALGORITHM': 'HS256',
--- cc_app/models.py
+++ cc_app/models.py
@@ -11,6 +11,17 @@ class CustomUser(AbstractUser):
 class Comic(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000, null=False)
+#   thumbnail = models.URLField()
+#   images = models.ImageField()
+#   price = models.DecimalField()
 
 # class FavoritesList(models.Model):
-#     comic = ManyToMany
+#     comic = models.ManyToMany
+
+# class Order(models.Model):
+#      name = models.CharField()
+#      is_gift = models.BooleanField()
+#      gift_message = models.CharField(max_length=500)
+
+# class Wishlist(models.Model):
+#      user_id = models.IntegerField()
